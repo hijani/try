e841f92fb7b714e98ae39f1926d2b3480380bba8
hijani <hijan29714@gmail.com>
2022-09-15 11:36:10 +0000

    changes

--- db.php
+++ db.php
@@ -3,8 +3,6 @@
     $connection = mysqli_connect('localhost', 'root', '', 'try');
             if(!$connection) {
                 die("connection error" . " " . mysqli_connect_error());
-            } else {
-                echo "connected";
             }
 
 
