664d0f88811e6fc54b337a0a7117585a0d2df920
hijani <hijan29714@gmail.com>
2022-09-15 11:44:18 +0000

    changes

--- display_data.php
+++ display_data.php
@@ -6,13 +6,16 @@
         $username = $_POST['username'];
         $gender = $_POST['gender'];
 
+        function showAllData() {
+            global $connection;
+            $display_query = "SELECT * FROM login";
+            $display_query_connection = mysqli_query($connection, $display_query);
 
-        $display_query = "SELECT * FROM login";
-        $display_query_connection = mysqli_query($connection, $display_query);
-
-        while($row = mysqli_fetch_assoc($display_query_connection)) {
-            $id = $row['id'];
-            echo "<option value='$id'>$id</option>";
+        
+            while($row = mysqli_fetch_assoc($display_query_connection)) {
+                $id = $row['id'];
+                echo "<option value='$id'>$id</option>";
+            }
         }
     }
 
