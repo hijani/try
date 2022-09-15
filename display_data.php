f8c4002780fc177cd7974e83d39eeb243706e978
hijani <hijan29714@gmail.com>
2022-09-15 11:34:10 +0000

    changes

---
+++ display_data.php
@@ -0,0 +1,23 @@
+<?php
+
+    if (isset($_POST['submit'])) {
+
+        $username = $_POST['username'];
+        $gender = $_POST['gender'];
+
+
+        $display_query = "SELECT * FROM login";
+        $display_query_connection = mysqli_query($connection, $display_query);
+
+        while($row = mysqli_fetch_assoc($display_query_connection)) {
+            ?>
+            <pre>
+            <?php 
+                print_r($row);
+            ?>
+
+            <?php
+        }
+    }
+
+?>
---
+++ insert_data.php
@@ -0,0 +1,19 @@
+<?php 
+
+    if (isset($_POST['submit'])) {
+
+        $username = $_POST['username'];
+        $gender = $_POST['gender'];
+
+
+        $insert_query = "INSERT INTO login(username, gender)";
+        $insert_query .= "VALUES('$username', $gender)";
+        $insert_query_connection = mysqli_query($connection, $insert_query);
+
+        if(!$insert_query_connection) {
+            die('connection error');
+        }
+
+    }
+
+?>
